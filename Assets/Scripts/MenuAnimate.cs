545dee5d9bda5848fd16673f9e87c24508583585
atmospherium <atmospherium@gmail.com>
2013-12-26 22:40:35 -0500

    Menu aesthetic upgrade

--- Assets/Scripts/MenuAnimate.cs
+++ Assets/Scripts/MenuAnimate.cs
@@ -9,15 +9,31 @@ public class MenuAnimate : MonoBehaviour {
 	int qSamples = 4096;
 	private float[] samples;
 
+	Material backgroundMaterial;
+	Vector2 backgroundDest = new Vector2(0f,0f);
+
+	GameObject lightObject;
+
 	// Use this for initialization
 	void Start () {
 		mainCamera = GameObject.Find("Main Camera");
+		lightObject = GameObject.Find ("MenuLight");
+		backgroundMaterial = GameObject.Find ("BackgroundSquare").renderer.sharedMaterial;
 		samples = new float[qSamples];
-		colorTarget = renderer.material.GetColor("_TintColor");
+		colorTarget = renderer.sharedMaterial.GetColor("_TintColor");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(Vector2.Distance(backgroundMaterial.mainTextureOffset,backgroundDest)<0.01f){
+			Debug.Log("Matched");
+			backgroundDest = new Vector2(Random.Range(0f,1f),Random.Range(0f,1f));
+		}
+		Vector2 textureOffset = Vector2.Lerp(backgroundMaterial.mainTextureOffset,backgroundDest,0.001f);
+		backgroundMaterial.SetTextureOffset("_BumpMap",textureOffset);
+		backgroundMaterial.mainTextureOffset = textureOffset;
+
 		Color currentColor = renderer.material.GetColor("_TintColor");
 
 		float floorRange = 200;
@@ -29,6 +45,8 @@ public class MenuAnimate : MonoBehaviour {
 		float alpha = (vol*vol*vol)+0.5f;
 		colorTarget = new Color(blur,blur,blur,alpha);
 		renderer.material.SetColor("_TintColor",colorTarget);
+
+		lightObject.light.intensity = Mathf.Pow((1+Mathf.Pow (blur,5)),5);
 
 	}
 
--- Assets/Scripts/MenuLight.cs
+++ Assets/Scripts/MenuLight.cs
@@ -23,15 +23,18 @@ public class MenuLight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Vector3.Distance(destination,transform.position)<0.05){
+		if(Vector3.Distance(destination,transform.localPosition)<0.05){
 			destination = new Vector3(Random.Range(lightCenter.x+xRange,lightCenter.x-xRange),
-			                          Random.Range(lightCenter.z+xRange,lightCenter.y-yRange),
+			                          Random.Range(lightCenter.y+xRange,lightCenter.y-yRange),
 			                          Random.Range(lightCenter.z+xRange,lightCenter.z-zRange));
 		}
 		transform.localPosition = Vector3.Lerp(transform.localPosition, destination, 0.01f);
+
+		/*
 		if(Random.Range(0f,1f)>0.8f){
 			intensityDestination = Random.Range(1.75f,2.15f);
 		}
 		light.intensity = Mathf.Lerp(light.intensity, intensityDestination*1.5f, 0.5f);
+		//*/
 	}
 }
